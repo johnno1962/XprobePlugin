07899f6ee8e7cf3b5c81438cd39bc22fb221ea59
John Holdsworth <github@johnholdsworth.com>
2017-10-31 22:14:06 +0000

    Xcode 9.1

--- XprobeSwift/XprobeSwift/Internals.swift
+++ XprobeSwift/XprobeSwift/Internals.swift
@@ -9,51 +9,51 @@
 import Foundation
 
 /** pointer to a function implementing a Swift method */
-typealias SIMP = @convention(c) (_: AnyObject) -> Void
+public typealias SIMP = @convention(c) (_: AnyObject) -> Void
 
 /**
  Layout of a class instance. Needs to be kept in sync with ~swift/include/swift/Runtime/Metadata.h
  */
-struct ClassMetadataSwift {
+public struct ClassMetadataSwift {
 
-    let MetaClass: uintptr_t = 0, SuperClass: uintptr_t = 0
-    let CacheData1: uintptr_t = 0, CacheData2: uintptr_t = 0
+    public let MetaClass: uintptr_t = 0, SuperClass: uintptr_t = 0
+    public let CacheData1: uintptr_t = 0, CacheData2: uintptr_t = 0
 
-    let Data: uintptr_t = 0
+    public let Data: uintptr_t = 0
 
     /// Swift-specific class flags.
-    let Flags: UInt32 = 0
+    public let Flags: UInt32 = 0
 
     /// The address point of instances of this type.
-    let InstanceAddressPoint: UInt32 = 0
+    public let InstanceAddressPoint: UInt32 = 0
 
     /// The required size of instances of this type.
     /// 'InstanceAddressPoint' bytes go before the address point;
     /// 'InstanceSize - InstanceAddressPoint' bytes go after it.
-    let InstanceSize: UInt32 = 0
+    public let InstanceSize: UInt32 = 0
 
     /// The alignment mask of the address point of instances of this type.
-    let InstanceAlignMask: UInt16 = 0
+    public let InstanceAlignMask: UInt16 = 0
 
     /// Reserved for runtime use.
-    let Reserved: UInt16 = 0
+    public let Reserved: UInt16 = 0
 
     /// The total size of the class object, including prefix and suffix
     /// extents.
-    let ClassSize: UInt32 = 0
+    public let ClassSize: UInt32 = 0
 
     /// The offset of the address point within the class object.
-    let ClassAddressPoint: UInt32 = 0
+    public let ClassAddressPoint: UInt32 = 0
 
     /// An out-of-line Swift-specific description of the type, or null
     /// if this is an artificial subclass.  We currently provide no
     /// supported mechanism for making a non-artificial subclass
     /// dynamically.
-    let Description: uintptr_t = 0
+    public let Description: uintptr_t = 0
 
     /// A function for destroying instance variables, used to clean up
     /// after an early return from a constructor.
-    var IVarDestroyer: SIMP? = nil
+    public var IVarDestroyer: SIMP? = nil
 
     // After this come the class members, laid out as follows:
     //   - class members for the superclass (recursively)
@@ -76,7 +76,7 @@ func _stdlib_demangleImpl(
     flags: UInt32
     ) -> UnsafeMutablePointer<CChar>?
 
-func _stdlib_demangleName(_ mangledName: String) -> String {
+public func _stdlib_demangleName(_ mangledName: String) -> String {
     return mangledName.utf8CString.withUnsafeBufferPointer {
         (mangledNameUTF8) in
 
