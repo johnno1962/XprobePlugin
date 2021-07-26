d80ac3dd12d37ca6e762fa710de49475b42142d5
John Holdsworth <github@johnholdsworth.com>
2021-07-26 19:16:40 +0200

    Re-instate interpose of most recent dylib.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/johnno1962/SwiftTrace",
-                 .upToNextMinor(from: "7.4.3")),
+                 .upToNextMinor(from: "7.5.0")),
     ],
     targets: [
         .target(name: "Xprobe", dependencies: ["XprobeSwift"]),
